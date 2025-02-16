4839c846b0e101bf245e85d2fe56d2853cdc964e
Edward <thanhtkcb2004@gmail.com>
2025-02-16 16:24:42 +0700

    search bar

---
+++ frontend/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import "./SearchBar.css"
+import search from "../../../assets/search.svg";
+const SearchBar = () => {
+    const [location,setLocation]=useState("");
+    const [checkIn,setCheckIn]=useState(new Date());
+    const [checkOut,setCheckOut]=useState(new Date());
+    const locations=["Hà Nội", "Đà Nẵng"];
+    return (
+        <div id="container">
+            <div id="location" className="search-box">
+                <h3>Địa điểm</h3>
+                <select onChange={(e) => setLocation(e.target.value)} >
+                    <option value="" disabled>Chọn địa điểm</option>
+                    {locations.map((loc)=>(
+                        <option key={loc} value={loc}>{loc}</option>
+                    ))}
+                </select>
+            </div>
+
+        <div id="time" className="search-box">
+            <h3>Thời gian</h3>
+            <div className="inp">
+            <input type="datetime-local" />
+            <span> - </span>
+            <input type="datetime-local" />
+            </div>
+        </div>
+
+        <div id="guest" className="search-box">
+            <h3>Số người</h3>
+            <input type="number" min="1" />
+        </div>
+
+        <button className="search-box search-button">
+            <img src={search} alt="search" />
+        </button>
+    </div>
+    )
+}
+export default SearchBar
--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import './HomePage.css'
+import SearchBar from '../../Components/SearchBar/SearchBar'
 const HomePage = () => {
     const ksData=[
         {hinhAnh:"",tenKhachSan:"KhachSan1",diemSoTB:"7.5"},
@@ -6,37 +7,30 @@ const HomePage = () => {
         {hinhAnh:"",tenKhachSan:"KhachSan1",diemSoTB:"7.5"},
         {hinhAnh:"",tenKhachSan:"KhachSan1",diemSoTB:"7.5"}
     ]
+    const kmData=[
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024"},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024"},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024"},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024"}
+    ]
     return (
-        <div id="voucher">
+        <div id="home">
+            <SearchBar/>
             <div style={{display:"flex"}}>
                 <h1>Khuyến mãi mới</h1>
                 <a href="/khuyenmai">Xem thêm &#8594;</a>
             </div>
             <div id="kmBoxs">
-                <div>
-                    <img src="/assets/khuyenmai.webp" alt="km"/>
-                    <p style={{fontWeight:"bold"}}>KhuyenMai1</p>
-                    <p>Thoi gian khuyen mai</p>
-                    <p></p>
-                </div>
-                <div>
-                    <img src="/assets/khuyenmai.webp" alt="km"/>
-                    <p style={{fontWeight:"bold"}}>KhuyenMai1</p>
-                    <p>Thoi gian khuyen mai</p>
-                    <p></p>
-                </div>
-                <div>
-                    <img src="/assets/khuyenmai.webp" alt="km"/>
-                    <p style={{fontWeight:"bold"}}>KhuyenMai1</p>
-                    <p>Thoi gian khuyen mai</p>
-                    <p></p>
-                </div>
-                <div>
-                    <img src="/assets/khuyenmai.webp" alt="km"/>
-                    <p style={{fontWeight:"bold"}}>KhuyenMai1</p>
-                    <p>Thoi gian khuyen mai</p>
-                    <p></p>
-                </div>
+                {
+                    kmData.map((item,index)=>(
+                        <div key={index}>
+                            <img src="/assets/khuyenmai.webp" alt="km"/>
+                            <p style={{fontWeight:"bold"}}>{item.maKhuyenMai}</p>
+                            <p>Thời gian khuyến mãi</p>
+                            <p style={{fontWeight:"bold"}}>{item.thoiGian}</p>
+                        </div>
+                    ))
+                }
             </div>
             <div id="hotel">
                 <h1>Khách sạn nổi bật</h1>
