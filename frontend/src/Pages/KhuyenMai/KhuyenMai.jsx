94e9511f5a697e53763f2a9fe682660804e0dbac
Edward <thanhtkcb2004@gmail.com>
2025-02-18 23:42:24 +0700

    Update

--- frontend/src/Pages/KhuyenMai/KhuyenMai.jsx
+++ frontend/src/Pages/KhuyenMai/KhuyenMai.jsx
@@ -1,7 +1,64 @@
+
+import './KhuyenMai.css'
 const KhuyenMai = () => {
+    const kmData=[
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000},
+        {maKhuyenMai:"0123525",thoiGian:"20-12-2024 to 22-12-2024",mucKhuyenMai:50,giaoDichToiThieu:5000000}
+    ]
+    //const reverse=
     return (
         <div>
-            khuyenMai
+            <h2 >Khuyến mãi mới nhất</h2>
+            <div id="kmBoxs">
+                {
+                    kmData.map((item,index)=>(
+                        <div key={index} id='km'>
+                            <img src="/assets/khuyenmai.webp" alt="km"/>
+                            <div style={{fontWeight:"bold",fontSize:25}}>{item.maKhuyenMai}</div>
+                            <div style={{display:"flex",gap:"1vw"}}>
+                                <div>
+                                    <p>Thời gian khuyến mãi</p>
+                                    <p style={{fontWeight:"bold"}}>{item.thoiGian}</p>
+                                </div>
+                                <div>
+                                    <p>Giao dịch tối thiểu</p>
+                                    <p style={{fontWeight:"bold"}}>{item.giaoDichToiThieu.toLocaleString("vi-VN")} VNĐ</p>
+                                </div>
+                            </div>
+                            <div>Mức khuyến mãi: <span style={{fontWeight:"bolder",fontSize:20,color:"rgb(1, 34, 63)"}}>{item.mucKhuyenMai} %</span></div>
+                        </div>
+                    ))
+                }
+            </div>
+   
+            <h2>Khuyến mãi sắp hết hạn</h2>
+            <div id="kmBoxs">
+                {
+                    kmData.map((item,index)=>(
+                        <div key={index} id='km'>
+                            <img src="/assets/khuyenmai.webp" alt="km"/>
+                            <div style={{fontWeight:"bold",fontSize:25}}>{item.maKhuyenMai}</div>
+                            <div style={{display:"flex",gap:"1vw"}}>
+                                <div>
+                                    <p>Thời gian khuyến mãi</p>
+                                    <p style={{fontWeight:"bold"}}>{item.thoiGian}</p>
+                                </div>
+                                <div>
+                                    <p>Giao dịch tối thiểu</p>
+                                    <p style={{fontWeight:"bold"}}>{item.giaoDichToiThieu.toLocaleString("vi-VN")} VNĐ</p>
+                                </div>
+                            </div>
+                            <div>Mức khuyến mãi: <span style={{fontWeight:"bolder",fontSize:20,color:"rgb(1, 34, 63)"}}>{item.mucKhuyenMai} %</span></div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- frontend/src/Pages/MyDC/MyDC.jsx
+++ frontend/src/Pages/MyDC/MyDC.jsx
@@ -1,7 +1,40 @@
+import "./MyDC.css";
 const MyDC = () => {
+    const data=[
+        {loaiPhong:"ABC",hinhAnh:"https://s3.eu-west-2.amazonaws.com/staticgh.gentinghotel.co.uk/uploads/hero/SuiteNov2022_0008_1920.jpg",thoiGian:"20-12-2024 to 22-12-2024",tongChiPhi:1000000,dienTich:5.6,tienIch:"abc",soNguoi:3},
+        {loaiPhong:"ABC",hinhAnh:"https://s3.eu-west-2.amazonaws.com/staticgh.gentinghotel.co.uk/uploads/hero/SuiteNov2022_0008_1920.jpg",thoiGian:"20-12-2024 to 22-12-2024",tongChiPhi:1000000,dienTich:5.6,tienIch:"abc",soNguoi:3},
+        {loaiPhong:"ABC",hinhAnh:"https://s3.eu-west-2.amazonaws.com/staticgh.gentinghotel.co.uk/uploads/hero/SuiteNov2022_0008_1920.jpg",thoiGian:"20-12-2024 to 22-12-2024",tongChiPhi:1000000,dienTich:5.6,tienIch:"abc",soNguoi:3},
+        {loaiPhong:"ABC",hinhAnh:"https://s3.eu-west-2.amazonaws.com/staticgh.gentinghotel.co.uk/uploads/hero/SuiteNov2022_0008_1920.jpg",thoiGian:"20-12-2024 to 22-12-2024",tongChiPhi:1000000,dienTich:5.6,tienIch:"abc",soNguoi:3}
+    ]
+    const isKM=true;
     return (
         <div>
-            đặt chỗ của tôi
+            <h2>Các phòng đã đặt</h2>
+            <i style={{marginTop:"1vh",marginLeft:"1vw"}}>*Các phòng có thể được huỷ miễn phí trước 1 tuần</i>
+            <div id="roomContainer">
+                {data.map((item,index)=>(<div key={index} id="room">
+                        <div id="rp1">
+                            <img src={item.hinhAnh} alt="room" />
+                        </div>
+                        <div id="rp2">
+                            <h2>{item.loaiPhong}</h2>
+                            <div>Diện tích: {item.dienTich}</div>
+                            <div>Tiện nghi: {item.tienIch}</div>
+                            <div>Số người: {item.soNguoi}</div>
+                            <div>Thời gian: {item.thoiGian}</div>
+                        </div>
+                        <div id="rp3">
+                            {isKM&&(
+                                <div>Đã áp dụng khuyến mãi</div>
+                            )}
+                            <h3>{item.tongChiPhi.toLocaleString("vi-VN")} VNĐ</h3>
+                        </div>
+                        <div id="rp4">
+                            <button onClick={()=>alert("Huỷ thành công")}>Huỷ đặt phòng</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
