208ff58abf9414784cbebbac51e934c03c942b29
Edward Laurent <thanhtkcb2004@gmail.com>
2025-06-08 22:55:18 +0700

    Delete backend/src/main/java/com/example/demo/Services/KhachSanService.java

--- backend/src/main/java/com/example/demo/Services/KhachSanService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Services;
-
-import com.example.demo.DTO.KhachSanChiTietDTO;
-
-public interface KhachSanService {
-    KhachSanChiTietDTO xemChiTietKS(String maKhachSan);
-}
